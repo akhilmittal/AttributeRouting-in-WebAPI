f5b2b6d3e51513948c77cc0aa8ba3256b4f33a52
Akhil Mittal <akhil.mittal20@gmail.com>
2015-05-31 15:08:35 +0530

    Source Code for Attribute Routing in WebAPI
    Source Code for Attribute Routing in WebAPI

---
+++ BusinessServices/DependencyResolver.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using DataModel;
+using DataModel.UnitOfWork;
+using Resolver;
+
+namespace BusinessServices
+{
+    [Export(typeof(IComponent))]
+    public class DependencyResolver : IComponent
+    {
+        public void SetUp(IRegisterComponent registerComponent)
+        {
+            registerComponent.RegisterType<IProductServices, ProductServices>();
+
+        }
+    }
+}
